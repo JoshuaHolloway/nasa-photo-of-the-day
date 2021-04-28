96e9c8ed7284a3a8f05098e0bd7e2a760092bc69
JoshuaHolloway <jhollow6@asu.edu>
2021-04-27 21:52:12 -0500

    ported scaling

--- src/Card.js
+++ src/Card.js
@@ -110,14 +110,22 @@ class Card extends React.Component {
           };
 
           // ========================================
-
-          const {viewport_center_x, viewport_center_y, viewport_width, viewport_height} = viewport_geometry();
-          const {x0, y0, w, h} = element_geometry(this.DOM_node_reference);
-          const {shift_x, shift_y} = get_center_shifts(viewport_center_x, viewport_center_y, x0, y0);
-
-
+          
           if (this.state.count === 0) {
+            
+            const {viewport_center_x, viewport_center_y, viewport_width, viewport_height} = viewport_geometry();
+            const {x0, y0, w, h} = element_geometry(this.DOM_node_reference);
+            const {shift_x, shift_y} = get_center_shifts(viewport_center_x, viewport_center_y, x0, y0);
+
+            let s;
+            const final_gap = 30;
+            if (viewport_width < viewport_height) // Scale smaller of two dimensions
+              s = (viewport_width - 2*final_gap) / w;
+            else
+              s = (viewport_height - 2*final_gap) / h;
+          
             this.timeline.to(this.DOM_node_reference, {x: shift_x, y: shift_y}, '<');
+            this.timeline.to(this.DOM_node_reference, {scale: s}, '<+0.1');
             this.timeline.play();
           }
           else
